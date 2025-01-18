9a682b45e833231e72a223d204ce92af8efe4263
InventEverything <ian.engle@student.centralia.edu>
2025-01-17 18:30:23 -0800

    Added basic user interface template

--- Vending_Machine/Program.cs
+++ Vending_Machine/Program.cs
@@ -4,6 +4,17 @@
     {
         static void Main(string[] args)
         {
+            ConsoleKey UserInput = ConsoleKey.None;
+            do
+            {
+                Console.WriteLine("Please press a number to make a selection: \n" +
+                    "(1) Beverage \n" +
+                    "(2) Snack \n" +
+                    "(3) Meal \n" +
+                    "(4) Refund \n");
+                UserInput = Console.ReadKey().Key;
+                Console.Clear();
+            } while (true);
         }
     }
 }
